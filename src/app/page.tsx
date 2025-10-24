90888145510bbcde613aa5f85a783a4d9dfe58d7
Gabriel Gomes <gbgta29@gmail.com>
2025-10-24 16:05:26 +0000

    troque o entregavel: https://tranquil-hummingbird-1d25e8.netlify.app/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { useState, useEffect, useRef } from "react";
@@ -425,7 +424,7 @@ export default function Home() {
           {flowStep === 'flow_complete_video_only' && (
              <div className="p-4 bg-background border-t border-border/20 flex justify-center">
               <Button asChild className="w-full bg-accent text-accent-foreground font-bold text-lg py-6 rounded-full shadow-lg hover:bg-accent/90">
-                <Link href="https://marvelous-parfait-3475a8.netlify.app/" target="_blank">
+                <Link href="https://tranquil-hummingbird-1d25e8.netlify.app/" target="_blank">
                   Iniciar chamada de vídeo
                 </Link>
               </Button>
@@ -438,15 +437,5 @@ export default function Home() {
     </div>
   );
 }
-
-    
-
-    
-
-    
-
-    
-
-    
 
     
